621f5c9d9bf278e7aee17782d45fc946d9ffd72f
diandiantong <tongjiny@sina.cn>
2018-10-23 17:57:31 +0800

    编辑添加课程视图

---
+++ src/views/course/upCourse/addTeacher.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-form :model="data" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <!--<el-form-item label="名师照片" required>
+      </el-form-item>-->
+    </el-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        data:[]
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
