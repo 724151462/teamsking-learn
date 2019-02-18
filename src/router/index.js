a99121fa5684fc0557012c98cbe28534b3da0e24
unknown <wu.19941215>
2019-02-18 16:36:23 +0800

    侧边栏修改

--- src/router/index.js
+++ src/router/index.js
@@ -215,14 +215,14 @@ export const constantRouterMap = [{
           },
         ]
       },
-      {
-        path: 'appraisal',
-        component: _import('course/appraisal'),
-        name: '课程评价',
-        children: [
+      // {
+      //   path: 'appraisal',
+      //   component: _import('course/appraisal'),
+      //   name: '课程评价',
+      //   children: [
 
-        ]
-      },
+      //   ]
+      // },
       {
         path: 'test',
         component: Layout,
@@ -306,9 +306,11 @@ export const constantRouterMap = [{
         ],
       },
       {
-        path: 'controller',
+        path: 'notice',
         component: Layout,
-        name: 'school管理员1',
+        name: '消息管理',
+        leaf: true,
+        menuShow: true,
         redirect: '/school/notice',
         children: [{
           path: '/school/notice',
@@ -327,36 +329,36 @@ export const constantRouterMap = [{
         children: [{
             path: '/school/portal/mould',
             component: _import('school/portal'),
-            name: 'school模板配置',
+            name: '模板配置',
             menuShow: true,
             children: [{
                 path: '/school/portal/mould',
                 component: _import('school/portalMould'),
-                name: 'school模板配置',
+                name: '模板配置',
                 menuShow: true,
               },
               {
                 path: '/school/portal/platform',
                 component: _import('school/portalPlatform'),
-                name: 'school平台信息',
+                name: '平台信息',
                 menuShow: true,
               },
               {
                 path: '/school/portal/banner',
                 component: _import('school/portalBanner'),
-                name: 'school轮播图',
+                name: '轮播图',
                 menuShow: true,
               },
               {
                 path: '/school/portal/recommend',
                 component: _import('school/portalRecommend'),
-                name: 'school课程推荐',
+                name: '课程推荐',
                 menuShow: true,
               },
               {
                 path: '/school/portal/teacher',
                 component: _import('school/portalTeacher'),
-                name: 'school名师风采',
+                name: '名师风采',
                 menuShow: true,
               }
             ]
@@ -364,25 +366,25 @@ export const constantRouterMap = [{
           {
             path: '/school/portal/platform',
             component: _import('school/portalPlatform'),
-            name: 'school平台信息',
+            name: '平台信息',
             menuShow: true,
           },
           {
             path: '/school/portal/banner',
             component: _import('school/portalBanner'),
-            name: 'school轮播图',
+            name: '轮播图',
             menuShow: true,
           },
           {
             path: '/school/portal/recommend',
             component: _import('school/portalRecommend'),
-            name: 'school课程推荐',
+            name: '课程推荐',
             menuShow: true,
           },
           {
             path: '/school/portal/teacher',
             component: _import('school/portalTeacher'),
-            name: 'school名师风采',
+            name: '名师风采',
             menuShow: true,
           }
         ]
@@ -390,24 +392,24 @@ export const constantRouterMap = [{
       {
         path: 'course',
         component: Layout,
-        name: 'school课程管理',
+        name: '课程管理',
         redirect: "/school/course/course",
         children: [{
             path: '/school/course/course',
             component: _import('school/course'),
-            name: 'school课程管理',
+            name: '课程管理',
             menuShow: true,
           },
           {
             path: '/school/course/certificate',
             component: _import('school/certificate'),
-            name: 'school证书审核',
+            name: '证书审核',
             menuShow: true,
           },
           {
             path: '/school/course/courseReview',
             component: _import('school/courseReview'),
-            name: 'school课程包审核',
+            name: '课程包审核',
             menuShow: true,
           },
         ]
@@ -420,25 +422,25 @@ export const constantRouterMap = [{
         children: [{
             path: '/school/structure/academy',
             component: _import('school/academy'),
-            name: 'school院管理',
+            name: '院管理',
             menuShow: true,
           },
           {
             path: '/school/structure/department',
             component: _import('school/department'),
-            name: 'school系管理',
+            name: '系管理',
             menuShow: true,
           },
           {
             path: '/school/structure/speciality',
             component: _import('school/speciality'),
-            name: 'school专业管理',
+            name: '专业管理',
             menuShow: true,
           },
           {
             path: '/school/structure/class',
             component: _import('school/class'),
-            name: 'school班级管理',
+            name: '班级管理',
             menuShow: true,
           }
         ]
@@ -456,7 +458,7 @@ export const constantRouterMap = [{
       path: '/learn/inclass',
       // leaf: true,
       component: Layout,
-      name: 'learn学习管理',
+      name: '学习管理',
       menuShow: true,
       children: [{
           path: '/learn/inclass',
@@ -496,37 +498,37 @@ export const constantRouterMap = [{
       {
         path: '/user/message',
         component: _import('user/message'),
-        name: 'user消息管理'
+        name: '消息管理'
       },
       {
         path: '/user/operation',
         component: _import('user/operation'),
-        name: 'user操作记录'
+        name: '操作记录'
       },
       {
         path: '/user/buy',
         component: _import('user/buy'),
-        name: 'user购买记录'
+        name: '购买记录'
       },
       {
         path: '/user/income',
         component: _import('user/income'),
-        name: 'user收入记录'
+        name: '收入记录'
       },
       {
         path: '/user/withdraw',
         component: _import('user/withdraw'),
-        name: 'user申请提现'
+        name: '申请提现'
       },
       {
         path: '/user/feedback',
         component: _import('user/feedback'),
-        name: 'user意见反馈'
+        name: '意见反馈'
       },
       {
         path: '/user/certificate',
         component: _import('user/certificate'),
-        name: 'user我的证书'
+        name: '我的证书'
       }
     ]
   },
@@ -541,52 +543,52 @@ export const constantRouterMap = [{
     children: [{
         path: 'student',
         component: _import('school/student'),
-        name: 'user学生管理'
+        name: '学生管理'
       },
       {
         path: 'teacher',
         component: _import('school/teacher'),
-        name: 'user教师管理'
+        name: '教师管理'
       },
       {
         path: 'controller',
         component: _import('school/controller'),
-        name: 'user管理员'
+        name: '管理员'
       },
       {
         path: 'portal',
         component: _import('school/portal'),
-        name: 'user门户配置'
+        name: '门户配置'
       },
       {
         path: 'course',
         component: _import('school/course'),
-        name: 'user课程管理'
+        name: '课程管理'
       },
       {
         path: 'certificate',
         component: _import('school/certificate'),
-        name: 'user证书审核'
+        name: '证书审核'
       },
       {
         path: 'academy',
         component: _import('school/academy'),
-        name: 'user院管理'
+        name: '院管理'
       },
       {
         path: 'department',
         component: _import('school/department'),
-        name: 'user系管理'
+        name: '系管理'
       },
       {
         path: 'speciality',
         component: _import('school/speciality'),
-        name: 'user专业管理'
+        name: '专业管理'
       },
       {
         path: 'class',
         component: _import('school/class'),
-        name: 'user班级管理'
+        name: '班级管理'
       },
     ]
   },
@@ -607,13 +609,13 @@ export const constantRouterMap = [{
         children: [{
             path: '/system/tenantManager',
             component: _import('system/tenantManager'),
-            name: 'system租户管理',
+            name: '租户管理',
             menuShow: true
           },
           {
             path: '/system/tenantAccountManager',
             component: _import('system/tenantAccountManager'),
-            name: 'system租户账号管理',
+            name: '租户账号管理',
             menuShow: true
           },
         ]
@@ -655,19 +657,19 @@ export const constantRouterMap = [{
         children: [{
             path: '/system/student',
             component: _import('system/student'),
-            name: 'system学生管理',
+            name: '学生管理',
             menuShow: true
           },
           {
             path: '/system/teacher',
             component: _import('system/teacher'),
-            name: 'system教师管理',
+            name: '教师管理',
             menuShow: true
           },
           {
             path: '/system/school',
             component: _import('system/school'),
-            name: 'system学校管理员',
+            name: '学校管理员',
             menuShow: true
           },
           {
@@ -678,13 +680,13 @@ export const constantRouterMap = [{
           {
             path: '/system/manager',
             component: _import('system/system'),
-            name: 'system系统管理员',
+            name: '系统管理员',
             menuShow: true
           },
           {
             path: '/system/tag',
             component: _import('system/tag'),
-            name: 'system标签管理',
+            name: '标签管理',
             menuShow: true
           },
         ]
@@ -696,13 +698,13 @@ export const constantRouterMap = [{
         children: [{
             path: '/system/menu',
             component: _import('system/menu'),
-            name: 'system菜单管理',
+            name: '菜单管理',
             menuShow: true
           },
           {
             path: '/system/role',
             component: _import('system/role'),
-            name: 'system角色管理',
+            name: '角色管理',
             menuShow: true
           },
         ]
@@ -710,43 +712,43 @@ export const constantRouterMap = [{
       {
         path: 'portal',
         component: Layout,
-        name: 'system门户配置',
+        name: '门户配置',
         redirect: '/system/portal/mould',
         leaf: true,
         menuShow: true,
         children: [{
             path: '/system/portal/mould',
             component: _import('system/portal'),
-            name: 'system模板配置',
+            name: '模板配置',
             menuShow: true,
             children: [{
                 path: '/system/portal/mould',
                 component: _import('system/portalMould'),
-                name: 'system模板配置',
+                name: '模板配置',
                 menuShow: true,
               },
               {
                 path: '/system/portal/platform',
                 component: _import('system/portalPlatform'),
-                name: 'system平台信息',
+                name: '平台信息',
                 menuShow: true,
               },
               {
                 path: '/system/portal/banner',
                 component: _import('system/portalBanner'),
-                name: 'system轮播图',
+                name: '轮播图',
                 menuShow: true,
               },
               {
                 path: '/system/portal/recommend',
                 component: _import('system/portalRecommend'),
-                name: 'system课程推荐',
+                name: '课程推荐',
                 menuShow: true,
               },
               {
                 path: '/system/portal/teacher',
                 component: _import('system/portalTeacher'),
-                name: 'system名师风采',
+                name: '名师风采',
                 menuShow: true,
               }
             ]
@@ -754,25 +756,25 @@ export const constantRouterMap = [{
           {
             path: '/system/portal/platform',
             component: _import('system/portalPlatform'),
-            name: 'system平台信息',
+            name: '平台信息',
             menuShow: true,
           },
           {
             path: '/system/portal/banner',
             component: _import('system/portalBanner'),
-            name: 'system轮播图',
+            name: '轮播图',
             menuShow: true,
           },
           {
             path: '/system/portal/recommend',
             component: _import('system/portalRecommend'),
-            name: 'system课程推荐',
+            name: '课程推荐',
             menuShow: true,
           },
           {
             path: '/system/portal/teacher',
             component: _import('system/portalTeacher'),
-            name: 'system名师风采',
+            name: '名师风采',
             menuShow: true,
           }
         ]
